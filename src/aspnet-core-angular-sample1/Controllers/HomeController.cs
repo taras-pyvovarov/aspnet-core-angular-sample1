2310f7ca6b1971a028b7ba08bd0546ed8aa0cdb4
Taras Pyvovarov <taras.pyvovarov@gmail.com>
2017-02-22 15:40:33 +0200

    Added MVC

---
+++ src/aspnet-core-angular-sample1/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace aspnet_core_angular_sample1.Controllers
+{
+    [Route("")]
+    [Route("Home")]
+    public class HomeController : Controller
+    {
+        [Route("")]
+        [Route(nameof(Index))]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
