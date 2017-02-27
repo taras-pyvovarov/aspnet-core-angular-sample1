2ba0b116be16dbdf36b6c8e4b69ebe04e68a2f39
Taras Pyvovarov <taras.pyvovarov@gmail.com>
2017-02-27 18:52:35 +0200

    One import in angular app root page

--- src/aspnet-core-angular-sample1/ClientApp1/main.ts
+++ src/aspnet-core-angular-sample1/ClientApp1/main.ts
@@ -1,6 +1,7 @@
 ﻿//App entry point.
 //Bootstraps application root module (named AppModule by convension).
 
+import 'angular2-universal-polyfills/browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app1.module';
 
