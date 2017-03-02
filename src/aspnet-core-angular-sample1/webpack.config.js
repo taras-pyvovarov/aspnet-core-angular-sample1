a1cbd3b7bddc67d39142e8f68def737dba67b044
Taras Pyvovarov <taras.pyvovarov@gmail.com>
2017-03-02 18:51:42 +0200

    Added server side js build

--- src/aspnet-core-angular-sample1/webpack.config.js
+++ src/aspnet-core-angular-sample1/webpack.config.js
@@ -32,4 +32,36 @@ var clientBundleConfig = {
     devtool: 'inline-source-map',
 }
 
-module.exports = [clientBundleConfig];
+// Configuration for server-side (prerendering) bundle suitable for running in Node
+var serverBundleConfig = {
+    entry: { 'main-server': './ClientApp1/app1-main-server.ts' },
+    output: {
+        //libraryTarget: 'commonjs',
+        path: path.join(__dirname, './ClientApp1/dist'),
+        filename: '[name].js',
+    },
+
+    resolve: {
+        // Add `.ts` and `.tsx` as a resolvable extension.
+        extensions: ['.ts', '.tsx', '.js'] // note if using webpack 1 you'd also need a '' in the array as well
+    },
+
+    module: {
+        loaders: [
+            {
+                enforce: 'pre',
+                test: /\.js$/,
+                loader: "source-map-loader"
+            },
+            {
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+            }
+        ]
+    },
+
+    devtool: 'inline-source-map',
+}
+
+
+module.exports = [clientBundleConfig, serverBundleConfig];
